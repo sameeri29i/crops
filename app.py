d259a5382f4b2b347dc56f9aaec4e280d53373c1
sameeri29i <88615574+sameeri29i@users.noreply.github.com>
2021-10-03 20:31:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ st.markdown(
         display: flex;
     }
     .logo-text {
-        font-weight:500 !important;
+        font-weight:700 !important;
         font-size:35px !important;
         color: #ffffff !important;
         padding-top: 5px !important;
@@ -60,7 +60,7 @@ st.markdown(
     f"""
     <div class="container">
         <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
-        <p class="logo-text">  Institute Of Industrial Electronics Engineering</p>
+        <p class="logo-text"> <br> Institute Of Industrial Electronics Engineering</p>
     </div>
     """,
     unsafe_allow_html=True
