fe10b97ed958d15f23db708ff2916d517eef2b3d
sameeri29i <88615574+sameeri29i@users.noreply.github.com>
2021-10-04 00:55:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ if uploaded_file is not None:
     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
     resized = cv2.resize(opencv_image,(224,224))
     # Now do something with the image! For example, let's display it:
-    st.image(opencv_image, channels="RGB")
+    st.image(opencv_image, channels="RGB", use_column_width=True)
 
     resized = mobilenet_v2_preprocess_input(resized)
     img_reshape = resized[np.newaxis,...]
