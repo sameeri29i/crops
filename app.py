db2fa894b55c566b52a7f5dba1d73984727a539c
sameeri29i <88615574+sameeri29i@users.noreply.github.com>
2021-10-03 18:19:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,29 @@ from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_in
 #import os, sys
 #import numpy as np
 #from threading import Thread
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("https://seedworld.com/site/wp-content/uploads/2020/08/GettyImages-539944240-696x464.jpg");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('background.png')
 
 st.title("Crop Analysis")
 model = tf.keras.models.load_model("fypmodel3.h5")
